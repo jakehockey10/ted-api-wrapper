5b4b502cb9eac79b4abd0da604e0aa8caf97fb64
Jake Smith <jakehockey10@gmail.com>
2015-03-06 02:03:49 -0700

    made server.  had to fix main.js in talk-ideas

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ var app = express();
 var qs = require('qs');
 var talkIdeas = require('talks-ideas')('j79qn8wkwmc554g328k43pa6', 'json');
 
-//var yelp = require("yelp").createClient({
-//        consumer_key: "b8iV8U1TojhwQ2SLdpAkoQ",
-//        consumer_secret: "q7Ghgx2gLh-qH-vNYmpLkUSueTQ",
-//        token: "W2ia0PkM0POzY6K2CSaUrYv5XRINCThT",
-//        token_secret: "PyHb46utgiZLhhDsR_36KWVSuKk"
-//    })
-//    ;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,44 +14,47 @@ app.get('/', function(req, res) {
 });
 
 app.get('/search/talks/:ted_params', function(req, res) {
-    console.log("is this getting called?");
-    console.log(req.params.ted_params);
-    res.send(tedSearch('talks', req.params.ted_params));
+    var data = {q: req.params.ted_params, categories: 'talks'};
+    talkIdeas.search.search(data, function (error, response) {
+        res.send(response);
+    });
 });
 
 app.get('/search/playlists/:ted_params', function(req, res) {
-    res.send(tedSearch('playlists', req.params.ted_params));
+    talkIdeas.search.search({ q: req.params.ted_params, categories: 'playlists' }, function (error, response) {
+        res.send(response);
+    });
 });
 
 app.get('/search/quotes/:ted_params', function(req, res) {
-    res.send(tedSearch('quotes', req.params.ted_params));
+    talkIdeas.search.search({ q: req.params.ted_params, categories: 'quotes' }, function (error, response) {
+        res.send(response);
+    });
 });
 
 app.get('/search/blog_posts/:ted_params', function(req, res) {
-    res.send(tedSearch('blog_posts', req.params.ted_params));
+    talkIdeas.search.search({ q: req.params.ted_params, categories: 'blog_posts' }, function (error, response) {
+        res.send(response);
+    });
 });
 
-function tedSearch(categories, params) {
-    talkIdeas.search.search({ q: params, categories: categories }, function (error, response) {
-        //res.send(response);
-        return response;
+app.get('/talks', function(req, res) {
+    talkIdeas.talks.list({}, function(error, response) {
+        res.send(response.talks.length.toString());
     })
-}
-//app.get('/search/:yelp_params', function(req, res) {
-//
-//    console.log(qs.parse(req.params.yelp_params));
-//    // See http://www.yelp.com/developers/documentation/v2/search_api
-//    yelp.search(qs.parse(req.params.yelp_params), function(error, data) {
-//        res.send(data);
-//    });
-//});
+});
 
-//app.get('/business/:yelp_params', function(req, res) {
-//    // See http://www.yelp.com/developers/documentation/v2/business
-//    yelp.business(req.params.yelp_params, function(error, data) {
-//        res.send(data);
-//    });
-//});
+app.get('/talks/:id', function (req, res) {
+    talkIdeas.talks.query({id: req.params.id}, function(error, response) {
+        res.send(response);
+    })
+});
+
+app.get('/talks/:id/download', function (req, res) {
+    talkIdeas.talks.subtitledDownloads({id: req.params.id}, function(error, response) {
+        res.send(response);
+    })
+});
 
 app.set('port', (process.env.PORT || 3000));
 
